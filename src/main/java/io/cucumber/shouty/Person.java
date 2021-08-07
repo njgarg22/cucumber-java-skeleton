6c24148d184936e3bd6c50c5a198d33ab585b8ce
njgarg22 <njgarg22@gmail.com>
2021-08-07 14:50:01 +0530

    Add a Person class. Fix stepdefinitions to the point where test still fails

---
+++ src/main/java/io/cucumber/shouty/Person.java
@@ -0,0 +1,15 @@
+package io.cucumber.shouty;
+
+import java.util.List;
+
+public class Person {
+    public void moveTo(Integer distance) {
+    }
+
+    public void shout(String message) {
+    }
+
+    public List<String> getMessagesHeard() {
+        return null;
+    }
+}
--- src/test/java/io/cucumber/shouty/StepDefinitions.java
+++ src/test/java/io/cucumber/shouty/StepDefinitions.java
@@ -4,20 +4,29 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
 public class StepDefinitions {
+
+    private Person sean;
+    private Person lucy;
+    private String messageFromSean;
+
     @Given("Lucy is located {int} metres from Sean")
     public void lucy_is_located_metres_from_sean(Integer distance) {
-        System.out.println(distance);
-        throw new io.cucumber.java.PendingException();
+        sean = new Person();
+        lucy = new Person();
+        lucy.moveTo(distance);
     }
     @When("Sean shouts {string}")
-    public void sean_shouts(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void sean_shouts(String message) {
+        sean.shout(message);
+        messageFromSean = message;
     }
     @Then("Lucy hears Sean's message")
     public void lucy_hears_sean_s_message() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assertEquals(Arrays.asList(messageFromSean), lucy.getMessagesHeard());
     }
 }
