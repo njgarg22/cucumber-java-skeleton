28e1abce5fd4faa70f78730ea498ab9423840773
njgarg22 <njgarg22@gmail.com>
2021-08-08 18:55:14 +0530

    Remove incidental details from scenarios. Adjust step definitions accordingly

--- src/test/java/io/cucumber/shouty/StepDefinitions.java
+++ src/test/java/io/cucumber/shouty/StepDefinitions.java
@@ -18,8 +18,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class StepDefinitions {
 
+    private static final int DEFAULT_RANGE = 100;
+
     private String messageFromSean;
-    private Network network;
+    private Network network = new Network(DEFAULT_RANGE);
     private Map<String, Person> people;
 
     @Before
@@ -32,9 +34,19 @@ public class StepDefinitions {
         network = new Network(range);
     }
 
+    @Given("a person named {word}")
+    public void a_person_named(String name) throws Throwable {
+        people.put(name, new Person(name, 0, network));
+    }
+
     @Given("a person named {word} is located at {int}")
-    public void a_person_named(String name, int location) {
+    public void a_person_named(String name, int location) throws Throwable {
         people.put(name, new Person(name, location, network));
+    }
+
+    @When("Sean shouts")
+    public void sean_shouts() {
+        people.get("Sean").shout("Hello World");
     }
 
     @When("Sean shouts {string}")
@@ -52,5 +64,15 @@ public class StepDefinitions {
     public void larry_does_not_hear_Sean_s_message() throws Throwable {
         List<String> heardByLarry = people.get("Larry").getMessagesHeard();
         assertThat(heardByLarry, not(hasItem(messageFromSean)));
+    }
+
+    @Then("Lucy should hear a shout")
+    public void lucy_should_hear_a_shout() {
+        assertEquals(1, people.get("Lucy").getMessagesHeard().size());
+    }
+
+    @Then("Larry should not hear a shout")
+    public void larry_should_not_hear_a_shout() throws Throwable {
+        assertEquals(0, people.get("Larry").getMessagesHeard().size());
     }
 }
